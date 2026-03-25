0bf24a03c86498a2518f1a895fcb9e91b42352c6
Pengfei.Zhang <zhangpengfei@zhongbaounion.com>
2026-03-25 18:33:31 +0800

    Update the file management logic a bit.

--- luci-app-filemanager/htdocs/luci-static/resources/view/filemanager.js
+++ luci-app-filemanager/htdocs/luci-static/resources/view/filemanager.js
@@ -1,21 +1,48 @@
 'use strict';
 
-var fs = L.resolveDefault(import('fs'), null);
-var ui = L.resolveDefault(import('ui'), null);
+'require ui';
+'require fs';
+'require rpc';
+
+
+var callInstall = rpc.declare({
+    object: 'luci.filemanager',
+    method: 'install',
+    params: ['filename'],
+});
+
+var callRename = rpc.declare({
+    object: 'luci.filemanager',
+    method: 'rename',
+    params: ["oldName", 'newName'],
+    expect: { '': {} }
+});
+
+var callMkdir = rpc.declare({
+    object: 'luci.filemanager',
+    method: 'mkdirUpload',
+    params: ["uploadPath"],
+
+});
+
+var upload_path="/tmp/upload"
+var tmp_file="upload_tmp.tmp"
 
 return L.view.extend({
+    load: function() {
+        return Promise.all([
+            L.resolveDefault(callMkdir(upload_path), {}),
+        ]);
+    },
+
     refresh: function() {
         location.reload();
     },
 
-    render: function() {
+    render: function(modules) {
         var _this = this;
 
-        return Promise.all([
-            L.resolveDefault(fs.list('/tmp'), [])
-        ]).then(function(data) {
-            var files = data[0];
-
+        return fs.list(upload_path).then(function(files) {
             var body = E('div', { 'class': 'cbi-map' }, [
                 E('h2', _('Filer')),
                 E('div', { 'class': 'cbi-map-descr' }, _('Support uploading and installation of files in the /tmp directory'))
@@ -27,27 +54,38 @@ return L.view.extend({
                     E('button', {
                         'class': 'btn cbi-button-confirm',
                         'click': function(ev) {
-                            return ui.uploadFile('/tmp').then(function(res) {
-                                ui.addNotification(null, E('p', _('Upload Success: ') + res.name), 'info');
-                                _this.refresh();
+                            return ui.uploadFile(`${upload_path}/${tmp_file}`, ev.target).then(function(res) {
+                                return L.resolveDefault(callRename(`${upload_path}/${tmp_file}`, `${upload_path}/${res.name}`), {}).then(function(ret) {
+                                    if (ret.code === 0)
+                                        return _this.refresh();
+                                    else {
+                                        ui.addNotification(null, E('p', _('Failed to upload file: %s.').format(res.name)));
+                                        return L.resolveDefault(fs.remove(file), {});
+                                    }
+                                });
                             }).catch(function(e) {
-                                if (e.message !== 'Canceled') alert(_('Upload Failed: ') + e.message);
+                                if (e.message !== 'Canceled'){
+                                   ui.addNotification(null, E('p', e.message)); 
+                                }
                             });
                         }
                     }, _('Upload'))
                 ])
             ]);
 
+
             var table = E('table', { 'class': 'table cbi-section-table' }, [
                 E('tr', { 'class': 'tr cbi-section-table-titles' }, [
                     E('th', { 'class': 'th' }, _('FileName')),
                     E('th', { 'class': 'th' }, _('FileSize')),
                     E('th', { 'class': 'th' }, _('ModifyTime')),
+                    E('th', { 'class': 'th' }, _('FileAttrs')),
                     E('th', { 'class': 'th' }, _('Operate'))
                 ])
             ]);
 
-            files.forEach(function(file) {
+            (files || []).forEach(function(file) {
+                console.log(JSON.stringify(file))
                 if (file.type !== 'file') return;
 
                 var date = new Date(file.mtime * 1000);
@@ -60,12 +98,13 @@ return L.view.extend({
                     E('td', { 'class': 'td' }, file.name),
                     E('td', { 'class': 'td' }, (file.size / 1024).toFixed(2) + ' KB'),
                     E('td', { 'class': 'td' }, timeStr),
+                    E('td', { 'class': 'td' }, fs.stptomode(file.mode) || '----------'), 
                     E('td', { 'class': 'td' }, [
                         file.name.endsWith('.ipk') ? E('button', {
                             'class': 'btn cbi-button-action',
                             'click': function() {
                                 ui.showModal(_('Installing...'), [ E('p', { 'class': 'spinning' }, _('Please wait a moment...')) ]);
-                                return fs.exec('/bin/opkg', ['install', '/tmp/' + file.name]).then(function(res) {
+                                L.resolveDefault(callInstall(`${upload_path}/${file.name}`), {}).then(function(res) {
                                     ui.hideModal();
                                     ui.addNotification(null, E('pre', res.stdout || res.stderr || _('Finished')), 'info');
                                 });
@@ -77,7 +116,7 @@ return L.view.extend({
                             'style': 'margin-left: 5px',
                             'click': function() {
                                 if (!confirm(_('Delete this?'))) return;
-                                return fs.remove('/tmp/' + file.name).then(function() {
+                                return fs.remove(file).then(function() {
                                     _this.refresh();
                                 });
                             }
