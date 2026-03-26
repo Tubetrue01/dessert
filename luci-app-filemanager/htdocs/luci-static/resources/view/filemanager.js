3b8b47e618b2dcd2213c748b6a64f21622e3b8f1
Pengfei.Zhang <zhangpengfei@zhongbaounion.com>
2026-03-26 14:27:14 +0800

    luci-app-filemanager has been done!

--- luci-app-filemanager/htdocs/luci-static/resources/view/filemanager.js
+++ luci-app-filemanager/htdocs/luci-static/resources/view/filemanager.js
@@ -3,30 +3,73 @@
 'require ui';
 'require fs';
 'require rpc';
+'require request';
 
 
-var callInstall = rpc.declare({
+const callInstall = rpc.declare({
     object: 'luci.filemanager',
     method: 'install',
     params: ['filename'],
 });
 
-var callRename = rpc.declare({
+const callRename = rpc.declare({
     object: 'luci.filemanager',
     method: 'rename',
     params: ["oldName", 'newName'],
     expect: { '': {} }
 });
 
-var callMkdir = rpc.declare({
+const callMkdir = rpc.declare({
     object: 'luci.filemanager',
     method: 'mkdirUpload',
     params: ["uploadPath"],
 
 });
 
-var upload_path="/tmp/upload"
-var tmp_file="upload_tmp.tmp"
+const callRemove = rpc.declare({
+    object: 'luci.filemanager',
+    method: 'remove',
+    params: ["filename"],
+
+});
+
+const formatMode = function(mode) {
+        const res = [];
+        const types = {
+            0o140000: 's', // socket
+            0o120000: 'l', // symbolic link
+            0o100000: '-', // regular file
+            0o060000: 'b', // block device
+            0o040000: 'd', // directory
+            0o020000: 'c', // character device
+            0o010000: 'p'  // FIFO
+        };
+
+        const type = types[mode & 0o170000] || '?';
+        res.push(type);
+
+        const chars = ['r', 'w', 'x'];
+        for (let i = 6; i >= 0; i -= 3) {
+            let bits = (mode >> i) & 7;
+            res.push(bits & 4 ? 'r' : '-');
+            res.push(bits & 2 ? 'w' : '-');
+            res.push(bits & 1 ? 'x' : '-');
+        }
+
+        return res.join('');
+    }
+
+const timeFormater = function(mtime) {
+        const date = new Date(mtime * 1000);
+        const timeStr = String.format('%04d-%02d-%02d %02d:%02d:%02d',
+            date.getFullYear(), date.getMonth() + 1, date.getDate(),
+            date.getHours(), date.getMinutes(), date.getSeconds()
+        );
+        return timeStr;
+    }
+
+const upload_path="/tmp/upload"
+const tmp_file="upload_tmp.tmp"
 
 return L.view.extend({
     load: function() {
@@ -35,100 +78,165 @@ return L.view.extend({
         ]);
     },
 
-    refresh: function() {
-        location.reload();
-    },
-
     render: function(modules) {
-        var _this = this;
+        const _this = this;
 
-        return fs.list(upload_path).then(function(files) {
-            var body = E('div', { 'class': 'cbi-map' }, [
-                E('h2', _('Upload'))
-            ]);
+        _this.handleSaveApply = null;
+        _this.handleSave = null;
+        _this.handleReset = null;
 
-            var uploadSection = E('div', { 'class': 'cbi-section' }, [
-                E('div', { 'class': 'cbi-section-descr' }, _('Support uploading and installation of files in the /tmp directory')),
 
-                E('div', 
-                    { 'class': 'cbi-section-node',
-                        'style': 'flex-direction: row'
-                    }, 
-                 [
-                    E('label', {
-                     'class': 'cbi-value',
-                     'style': 'display:inline-block; width: 130px',
-                     }, '请选择文件：'),
-                   
-                   E('input', {
-                            'type': 'file',
-                            'class': 'cbi-input-file',
-                            'style': 'width: 400px',
-                            'change': function (ev) {
-                                var file = ev.target.files[0];
-                                if (file) {
-                                    console.log('选中的文件:', file.name);
+        const body = E('div', { 'class': 'cbi-map' }, []);
+
+        const updateTitle = E("h2", _('Upload'));
+
+        let fileInput = null; 
+
+        const uploadSection = E('div', { 'class': 'cbi-section' }, [
+            E('div', { 'class': 'cbi-section-descr' }, _('Support uploading and installation of files in the /tmp directory')),
+
+            E('div', 
+                { 'class': 'cbi-section-node',
+                  'style': 'flex-direction: row'
+                }, 
+             [
+                E('label', {
+                 'class': 'cbi-value',
+                 'style': 'display:inline-block; width: 130px; padding-bottom: 1rem',
+                 }, _('Please select a file: ')),
+               
+               E('input', {
+                        'type': 'file',
+                        'class': 'cbi-input-file',
+                        'style': 'width: 400px',
+                        'change': function(ev) {
+                            fileInput = ev.target.files[0] 
+                        }
+                }),
+
+               E('button', {
+                    class: 'cbi-button cbi-input-apply',
+                    click: function() {
+                        if (!fileInput) {
+                            return;
+                        }
+                        const data = new FormData();
+                        const targetPath = `${upload_path}/${tmp_file}`;  
+                        data.append('sessionid', rpc.getSessionID());
+                        data.append('filename', targetPath);
+                        data.append('filedata', fileInput);
+
+                        request.post(`${L.env.cgi_base}/cgi-upload`, data, {
+                            timeout: 0
+                        }).then(function(res) {
+                            return L.resolveDefault(callRename(`${upload_path}/${tmp_file}`, `${upload_path}/${fileInput.name}`), {}).then(function(ret) {
+                                if (ret.code === 0) {
+                                    fetchTableFiles();
+                                } else {
+                                    ui.addNotification(null, E('p', _('Failed to upload file.')));
                                 }
-                            }
-                    }),
+                            });
+                        }).catch(function(err) {
+                            ui.addNotification(null, E('p', err.message));
+                        });
+                    }
+                }, _('Upload'))
+            ]),
+        ]);
 
-                   E('input', {
-                            'type': 'submit',
-                            'class': 'cbi-button cbi-input-apply',
-                            'click': function (ev) {
-                                    console.log('选中的文件:', JSON.stringify(ev));
-                            }
-                    }),
+        const downloadTitle = E('h2', _('Download'));
 
+        const downloadSection = E('div', {'class': 'cbi-section'}, [
+            E('div', { 'class': 'cbi-section-descr' }, _('Download File')),
 
-                    // E('button', {
-                    //     'class': 'btn cbi-button-confirm',
-                    //     'click': function(ev) {
-                    //         return ui.uploadFile(`${upload_path}/${tmp_file}`, ev.target).then(function(res) {
-                    //             return L.resolveDefault(callRename(`${upload_path}/${tmp_file}`, `${upload_path}/${res.name}`), {}).then(function(ret) {
-                    //                 if (ret.code === 0)
-                    //                     return _this.refresh();
-                    //                 else {
-                    //                     ui.addNotification(null, E('p', _('Failed to upload file: %s.').format(res.name)));
-                    //                     return L.resolveDefault(fs.remove(file), {});
-                    //                 }
-                    //             });
-                    //         }).catch(function(e) {
-                    //             if (e.message !== 'Canceled'){
-                    //                ui.addNotification(null, E('p', e.message)); 
-                    //             }
-                    //         });
-                    //     }
-                    // }, _('Upload'))
-                ])
-            ]);
+            E('div',{
+                  'class': 'cbi-section-node',
+                  'style': 'flex-direction: row'
+            },[
+
+                E('label', {
+                 'class': 'cbi-value',
+                 'style': 'display:inline-block; width: 130px; padding-bottom: 1rem',
+                 }, _('Router root directory: ')),
 
 
-            var table = E('table', { 'class': 'table cbi-section-table' }, [
-                E('tr', { 'class': 'tr cbi-section-table-titles' }, [
-                    E('th', { 'class': 'th' }, _('FileName')),
-                    E('th', { 'class': 'th' }, _('FileSize')),
-                    E('th', { 'class': 'th' }, _('ModifyTime')),
-                    E('th', { 'class': 'th' }, _('FileAttrs')),
-                    E('th', { 'class': 'th' }, _('Operate'))
-                ])
-            ]);
+                E('input',{
+                    'type': 'text',
+                    'class': 'cbi-input-file',
+                    'style': 'width: 400px',
+                    'change': function(ev) {
+                        fileInput = ev.target.value;
+                    }
+                }),
 
-            (files || []).forEach(function(file) {
-                console.log(JSON.stringify(file))
-                if (file.type !== 'file') return;
+                E('input', {
+                        'type': 'submit',
+                        'class': 'cbi-button cbi-input-apply',
+                        'value': _('Download'),
+                        'click': function (ev) {
+                            rpc.call("file", "read", { path: fileInput }).then(function(content){
+                                const blob = new Blob([content], { type: "application/octet-stream" });
+                                const url = URL.createObjectURL(blob);
 
-                var date = new Date(file.mtime * 1000);
-                var timeStr = String.format('%04d-%02d-%02d %02d:%02d:%02d',
-                    date.getFullYear(), date.getMonth() + 1, date.getDate(),
-                    date.getHours(), date.getMinutes(), date.getSeconds()
-                );
+                                const fileName = fileInput.split('/').pop();
+                                const a = document.createElement("a");
+                                a.href = url;
+                                a.download = fileName;
+                                a.click();
 
-                table.appendChild(E('tr', { 'class': 'tr' }, [
+                                URL.revokeObjectURL(url);
+                            }).catch(function(err){
+                                ui.addNotification(null, E('p', _('Failed to download: ') + err.message));
+                            });
+
+                        }
+                })
+            ]),
+
+        ]);
+
+        let tableBody;
+        const table = E('table', { 'class': 'table cbi-section-table' }, [
+            E('tr', { 'class': 'tr cbi-section-table-titles' }, [
+                E('th', { 'class': 'th' }, _('FileName')),
+                E('th', { 'class': 'th' }, _('FileSize')),
+                E('th', { 'class': 'th' }, _('ModifyTime')),
+                E('th', { 'class': 'th' }, _('FileAttrs')),
+                E('th', { 'class': 'th' }, _('Remove')),
+                E('th', { 'class': 'th' }, _('Install'))
+            ]),
+            tableBody = E('tbody')
+        ]);
+
+        const tableSection = E('div', {'class': 'cbi-section'}, [
+            E('h3', _('FileList')),
+            table
+        ]);
+
+
+        const fetchTableFiles = () => fs.list(upload_path).then(function(files){
+            tableBody.innerHTML = ''; 
+
+            files.forEach(function(file){
+                if (file.type !== 'file') {
+                    return;
+                } 
+
+                tableBody.appendChild(E('tr', { 'class': 'tr' }, [
                     E('td', { 'class': 'td' }, file.name),
                     E('td', { 'class': 'td' }, (file.size / 1024).toFixed(2) + ' KB'),
-                    E('td', { 'class': 'td' }, timeStr),
-                    E('td', { 'class': 'td' }, fs.stptomode(file.mode) || '----------'), 
+                    E('td', { 'class': 'td' }, timeFormater(file.mtime)),
+                    E('td', { 'class': 'td' }, formatMode(file.mode)), 
+                    E('td', { 'class': 'td' }, [
+                        E('button', {
+                            'class': 'btn cbi-button-remove',
+                            'style': 'margin-left: 5px',
+                            'click': function() {
+                                return fs.remove(`${upload_path}/${file.name}`).then(() => fetchTableFiles());
+                            }
+                        }, _('Remove'))
+                    ]), 
+
                     E('td', { 'class': 'td' }, [
                         file.name.endsWith('.ipk') ? E('button', {
                             'class': 'btn cbi-button-action',
@@ -140,28 +248,20 @@ return L.view.extend({
                                 });
                             }
                         }, _('Install')) : '',
-
-                        E('button', {
-                            'class': 'btn cbi-button-remove',
-                            'style': 'margin-left: 5px',
-                            'click': function() {
-                                if (!confirm(_('Delete this?'))) return;
-                                return fs.remove(file).then(function() {
-                                    _this.refresh();
-                                });
-                            }
-                        }, _('Remove'))
+                       
                     ])
                 ]));
             });
-
-            body.appendChild(uploadSection);
-            body.appendChild(E('div', { 'class': 'cbi-section' }, [
-                E('h3', _('FileList')),
-                table
-            ]));
-
-            return body;
         });
+
+        fetchTableFiles();
+
+        body.appendChild(updateTitle);
+        body.appendChild(uploadSection);
+        body.appendChild(downloadTitle);
+        body.appendChild(downloadSection);
+        body.appendChild(tableSection);
+
+        return body;
     }
 });
