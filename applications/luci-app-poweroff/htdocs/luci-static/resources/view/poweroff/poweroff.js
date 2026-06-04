06361ba5c536435a128dbf13de81aa31807358eb
wangyuanyuan <Gladysdrea@gmail.com>
2026-06-04 23:49:56 +0800

    Align poweroff style with reboot.

--- applications/luci-app-poweroff/htdocs/luci-static/resources/view/poweroff/poweroff.js
+++ applications/luci-app-poweroff/htdocs/luci-static/resources/view/poweroff/poweroff.js
@@ -5,6 +5,7 @@
 'require ui';
 'require rpc';
 'require view';
+'require uci';
 
 const callPoweroff = rpc.declare({
     object: 'luci.poweroff',
@@ -12,58 +13,54 @@ const callPoweroff = rpc.declare({
     expect: {'': {}}
 });
 
-const getChanges = rpc.declare({
-    object: 'uci',
-    method: 'changes',
-    expect: {'changes': {}}
-});
+return view.extend({
+    load() {
+        return uci.changes();
+    },
 
-let confirmedOnce = false;
-
-return L.view.extend({
-
-    render() {
-        return E('div', {'class': 'cbi-map'}, [
+    render(changes) {
+        const body = E([
             E('h2', _('PowerOff')),
-            E('hr'),
-
-            E('div', {'class': 'cbi-section'}, [
-                E('div', {'class': 'cbi-section-descr'}, _('WARNING: May cause a reboot on a device that does not support power off.')),
-            ]),
-
-            E('div', {'class': 'cbi-page-actions'}, [
-                E('button', {
-                    'class': 'cbi-button cbi-button-reset important',
-                    'click': ui.createHandlerFn(this, 'handlePowerOff')
-                }, [_('Proceed')])
-            ])
+            E('p', {'style': 'padding-left: 1.5rem'}, _('WARNING: May cause a reboot on a device that does not support power off.'))
         ]);
+
+        if (changes && Object.keys(changes).length > 0) {
+            body.appendChild(E('p', {'class': 'alert-message warning', 'style': 'padding-left: 1.5rem'},
+                _('Warning: There are unsaved changes that will get lost on poweroff!')));
+        }
+
+        body.appendChild(E('hr'));
+        body.appendChild(E('div', {
+            'class': 'cbi-page-actions'
+        }, [
+            E('button', {
+                'class': 'btn danger',
+                'click': ui.createHandlerFn(this, 'handlePowerOff')
+            }, _('Proceed'))
+        ]));
+
+        return body;
+
     },
 
     handlePowerOff(ev) {
-        return getChanges().then(changes => {
-            if (!confirmedOnce && changes && Object.keys(changes).length > 0) {
-                ui.addNotification(null, E('p', _('Warning: There are unsaved changes that will get lost on poweroff!')), 'danger');
-                confirmedOnce = true;
-                return;
+        ui.changes.displayStatus(
+            'notice spinning',
+            E('p', {'class': 'spinning'}, _('Device is shutting down...'))
+        );
+
+        return callPoweroff().then(res => {
+            if (res && res.code === 0) {
+                window.setTimeout(() => {
+                    window.location.href = L.url('admin');
+                }, 10000);
+            } else if (res && res.code === -1) {
+                ui.addNotification(
+                    null,
+                    E('p', _('Warning: This device does not support powering off!')),
+                    'danger'
+                );
             }
-
-            ui.changes.displayStatus(
-                'notice spinning',
-                E('p', {'class': 'spinning'}, _('Device is shutting down...'))
-            );
-
-            return callPoweroff()
-                .then(res => {
-                    if (res && res.code === 0) {
-                        window.setTimeout(() => {
-                            window.location.href = L.url('admin');
-                        }, 10000);
-                    } else if (res && res.code === -1) {
-                        ui.changes.displayStatus(false);
-                        ui.addNotification(null, E('p', _('Warning: This device does not support powering off!')), 'danger');
-                    }
-                });
         });
     },
 
